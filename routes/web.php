f06d4b34a498e465cb46a1cf6f2a3debde61f093
Matheus438 <matheusbispoa3@gmail.com>
2024-12-19 00:12:23 -0300

    Inicio da Pagina Inicial

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\PaginaInicial;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('view/inicio', PaginaInicial::class, 'PaginaInicial');
